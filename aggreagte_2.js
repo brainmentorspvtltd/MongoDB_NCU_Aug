6091f91aaaa7dc5b7db547bcfeafbbe062b0b25d
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2021-09-17 11:38:44 +0530

    Add files via upload

--- aggreagte_2.js
+++ aggreagte_2.js
@@ -144,3 +144,41 @@ db.students.aggregate([
     {$group : {_id : "$_id", name : {$first : "$name"}, maxScore : {$max : "$score"}}},
     {$sort : {maxScore : -1}}
 ]).pretty()
+
+
+// buckets
+db.persons.aggregate([
+    {
+        $bucket : {
+            groupBy : "$dob.age",
+            boundaries : [10,20,40,50,60,70,80],
+            output : {
+                numPersons : {
+                    $sum : 1
+                },
+                averageAge : {
+                    $avg : "$dob.age"
+                }
+            }
+        }
+    }
+]).pretty()
+
+db.persons.aggregate([
+    {
+        $bucketAuto : {
+            groupBy : "$dob.age",
+            buckets : 5,
+            output : {
+                numPersons : {
+                    $sum : 1
+                },
+                averageAge : {
+                    $avg : "$dob.age"
+                }
+            }
+        }
+    }
+])
+
+
